b236caa07785bef55245fe0bcb86f8bcb1b661a6
jm12138 <2286040843@qq.com>
2021-04-21 00:21:39 +0800

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='ppim',
-    version='1.0.7',
+    version='1.0.8',
     author='jm12138',
     author_email='2286040843@qq.com',
     packages=['ppim', 'ppim.models', 'ppim.units'],
