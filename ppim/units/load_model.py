70014dc612b5f79e00c818f45e0b383e6f393348
jm12138 <2286040843@qq.com>
2021-05-08 22:19:08 +0800

    Update load_model.py

--- ppim/units/load_model.py
+++ ppim/units/load_model.py
@@ -1,7 +1,7 @@
-from paddle.utils.download import get_weights_path_from_url
+import paddle
 
 
 def load_model(model, url):
-    path = get_weights_path_from_url(url)
+    path = paddle.utils.download.get_weights_path_from_url(url)
     model.set_state_dict(paddle.load(path))
     return model
