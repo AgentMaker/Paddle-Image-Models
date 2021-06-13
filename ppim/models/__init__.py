dfb841a3ad817538abfa251ceccc94ae33640a18
jm12138 <2286040843@qq.com>
2021-06-13 17:04:29 +0800

    update __init__

--- ppim/models/__init__.py
+++ ppim/models/__init__.py
@@ -1,23 +1,23 @@
 # Transformer
-from .vit import VisionTransformer
-from .tnt import tnt_s, TNT
-from .t2t import t2t_vit_7, t2t_vit_10, t2t_vit_12, t2t_vit_14, t2t_vit_19, t2t_vit_24, t2t_vit_t_14, t2t_vit_t_19, t2t_vit_t_24, t2t_vit_14_384, t2t_vit_24_token_labeling
-from .pvt import pvt_ti, pvt_s, pvt_m, pvt_l, PyramidVisionTransformer
-from .pit import pit_ti, pit_s, pit_xs, pit_b, pit_ti_distilled, pit_s_distilled, pit_xs_distilled, pit_b_distilled, PoolingTransformer, DistilledPoolingTransformer
-from .coat import coat_ti, coat_m, coat_lite_ti, coat_lite_m, CoaT
-from .deit import deit_ti, deit_s, deit_b, deit_b_384, deit_ti_distilled, deit_s_distilled, deit_b_distilled, deit_b_distilled_384, DistilledVisionTransformer
-from .cait import cait_xxs_24, cait_xxs_36, cait_s_24, cait_xxs_24_384, cait_xxs_36_384, cait_xs_24_384, cait_s_24_384, cait_s_36_384, cait_m_36_384, cait_m_48_448, CaiT
-from .swin import swin_ti, swin_s, swin_b, swin_b_384, SwinTransformer
-from .levit import levit_128s, levit_128, levit_192, levit_256, levit_384, LeViT
-from .lvvit import lvvit_s, lvvit_s_384, lvvit_m, lvvit_m_384, lvvit_m_448, lvvit_l_448, LV_ViT
+from ppim.models.vit import VisionTransformer
+from ppim.models.tnt import tnt_s, TNT
+from ppim.models.t2t import t2t_vit_7, t2t_vit_10, t2t_vit_12, t2t_vit_14, t2t_vit_19, t2t_vit_24, t2t_vit_t_14, t2t_vit_t_19, t2t_vit_t_24, t2t_vit_14_384, t2t_vit_24_token_labeling
+from ppim.models.pvt import pvt_ti, pvt_s, pvt_m, pvt_l, PyramidVisionTransformer
+from ppim.models.pit import pit_ti, pit_s, pit_xs, pit_b, pit_ti_distilled, pit_s_distilled, pit_xs_distilled, pit_b_distilled, PoolingTransformer, DistilledPoolingTransformer
+from ppim.models.coat import coat_ti, coat_m, coat_lite_ti, coat_lite_m, CoaT
+from ppim.models.deit import deit_ti, deit_s, deit_b, deit_b_384, deit_ti_distilled, deit_s_distilled, deit_b_distilled, deit_b_distilled_384, DistilledVisionTransformer
+from ppim.models.cait import cait_xxs_24, cait_xxs_36, cait_s_24, cait_xxs_24_384, cait_xxs_36_384, cait_xs_24_384, cait_s_24_384, cait_s_36_384, cait_m_36_384, cait_m_48_448, CaiT
+from ppim.models.swin import swin_ti, swin_s, swin_b, swin_b_384, SwinTransformer
+from ppim.models.levit import levit_128s, levit_128, levit_192, levit_256, levit_384, LeViT
+from ppim.models.lvvit import lvvit_s, lvvit_s_384, lvvit_m, lvvit_m_384, lvvit_m_448, lvvit_l_448, LV_ViT
 
 # CNN
-from .dla import dla_34, dla_46_c, dla_46x_c, dla_60, dla_60x, dla_60x_c, dla_102, dla_102x, dla_102x2, dla_169, DLA
-from .cdnv2 import cdnv2_a, cdnv2_b, cdnv2_c, CondenseNetV2
-from .rexnet import rexnet_1_0, rexnet_1_3, rexnet_1_5, rexnet_2_0, rexnet_3_0, ReXNet
-from .repvgg import repvgg_a0, repvgg_a1, repvgg_a2, repvgg_b0, repvgg_b1, repvgg_b2, repvgg_b3, repvgg_b1g2, repvgg_b1g4, repvgg_b2g4, repvgg_b3g4, RepVGG
-from .hardnet import hardnet_68, hardnet_85, hardnet_39_ds, hardnet_68_ds, HarDNet
-from .rednet import rednet_26, rednet_38, rednet_50, rednet_101, rednet_152, RedNet
+from ppim.models.dla import dla_34, dla_46_c, dla_46x_c, dla_60, dla_60x, dla_60x_c, dla_102, dla_102x, dla_102x2, dla_169, DLA
+from ppim.models.cdnv2 import cdnv2_a, cdnv2_b, cdnv2_c, CondenseNetV2
+from ppim.models.rexnet import rexnet_1_0, rexnet_1_3, rexnet_1_5, rexnet_2_0, rexnet_3_0, ReXNet
+from ppim.models.repvgg import repvgg_a0, repvgg_a1, repvgg_a2, repvgg_b0, repvgg_b1, repvgg_b2, repvgg_b3, repvgg_b1g2, repvgg_b1g4, repvgg_b2g4, repvgg_b3g4, RepVGG
+from ppim.models.hardnet import hardnet_68, hardnet_85, hardnet_39_ds, hardnet_68_ds, HarDNet
+from ppim.models.rednet import rednet_26, rednet_38, rednet_50, rednet_101, rednet_152, RedNet
 
 # MLP
-from .mixer import mixer_b, mixer_l, MlpMixer
+from ppim.models.mixer import mixer_b, mixer_l, MlpMixer
